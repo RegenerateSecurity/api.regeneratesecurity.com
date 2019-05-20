ccf9ee2c64c8689180e5960229418e00cc6c62b2
holly <holly@localhost.localdomain>
2019-05-20 16:31:36 -0400

    Building up theme

--- api/1.0/hello.php
+++ api/1.0/hello.php
@@ -3,13 +3,13 @@ include_once $_SERVER['DOCUMENT_ROOT'] .  '/hmac.php';
 
 if (isset($_SERVER['HTTP_SIGNATURE'])) {
   $inputJSON = file_get_contents('php://input');
-  print bin2hex($inputJSON);
   $signature = hash_hmac('sha3-512' , $inputJSON , $apiHMAC);
   if ($signature == $_SERVER['HTTP_SIGNATURE']) {
     print '{"signature":"match"}';
   }
   else {
     print '{"signature":"no dice"}';
+    print $signature . '<br/>' . $_SERVER['HTTP_SIGNATURE'];
   }
   //$input = json_decode($inputJSON, TRUE); //convert JSON into array
 
