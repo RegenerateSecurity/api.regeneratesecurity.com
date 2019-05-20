d5af6c8e8e4325d0ee534c0a285af42a16a7fb93
holly <holly@localhost.localdomain>
2019-05-20 16:44:31 -0400

    Building up theme

--- api/1.0/hello.php
+++ api/1.0/hello.php
@@ -1,24 +1,22 @@
 <?php
+header('Content-Type: application/json');
 include_once $_SERVER['DOCUMENT_ROOT'] .  '/hmac.php';
 
 if (isset($_SERVER['HTTP_SIGNATURE'])) {
   $inputJSON = file_get_contents('php://input');
   $signature = hash_hmac('sha3-512' , $inputJSON , $apiHMAC);
   if ($signature == $_SERVER['HTTP_SIGNATURE']) {
-    print '{"signature":"match"}';
+    $input = json_decode($inputJSON, TRUE); //convert JSON into array
+    if ($input === null) {
+      print '{"error":"JSON Decode error"}';
+    }
+    else {
+      print '{"error":"false"}
+    }
   }
   else {
-    print '{"signature":"no dice"}';
-    print '<br />' . $signature . '<br/>' . $_SERVER['HTTP_SIGNATURE'];
+    print '{"signature":"mismatch"}';
   }
-  //$input = json_decode($inputJSON, TRUE); //convert JSON into array
-
-  //if ($input === null) {
-  //  print '{"error":"Error decoding json."}';
-  //}
-  //else {
-  //  print '{"error":"false"}';
-  //}
 }
 else {
   print '{"error":"Signature missing."}';
